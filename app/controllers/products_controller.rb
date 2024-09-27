59d9065ae93c199f57954e41df394be5edf598e3
BrunoSantosFF <brunosantosfernandes0@gmail.com>
2024-09-27 16:20:48 -0300

    create post

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,10 +12,25 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      Rails.logger.info "Produto criado com sucesso: #{@product.inspect}"
+      redirect_to products_url, notice: 'Produto criado com sucesso.'
+    else 
+      Rails.logger.warn "Falha ao criar produto: #{@product.errors.full_messages.join(', ')}"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :quantidade, :price)
   end
 
 end
