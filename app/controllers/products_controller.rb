663441f88d51fc1175b3b5893bf135bccf94070a
BrunoSantosFF <brunosantosfernandes0@gmail.com>
2024-09-27 14:28:05 -0300

    insert callback controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,16 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show]
+
   def index
     @products = Product.all
   end
 
   def show
+  end
+
+  private
+
+  def set_product
     @product = Product.find(params[:id])
   end
 
