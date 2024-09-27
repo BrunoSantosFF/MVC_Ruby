d581b379b8d695d24522d0acddc418e6a28aaf44
BrunoSantosFF <brunosantosfernandes0@gmail.com>
2024-09-27 16:29:56 -0300

    refresh index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'products',  to: "products#index"
-  get 'products/new', to: "products#new"
-  post 'products', to: "products#create"
-  get 'products/:id', to: "products#show"
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get 'products',  to: "products#index"
+  # get 'products/new', to: "products#new"
+  # post 'products', to: "products#create"
+  # get 'products/:id', to: "products#show"
+  # get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :products
 
   
 end
